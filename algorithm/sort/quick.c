88b442d3610fb13888f060cbd0bc6c3758f87379
Vincent <127123332+Charles-lab-ux@users.noreply.github.com>
2024-04-18 01:13:06 +0800

    Add files via upload

---
+++ algorithm/sort/quick.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void swap(int* a,int* b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int quickSort(int arr[],int l,int r){
+    if(l>=r)return 0;
+    int temp=arr[l],i=l,j=r;
+    while(i!=j){
+        while(i<j&&arr[j]>=temp)j--;
+        swap(&arr[i],&arr[j]);
+        while(i<j&&arr[i]<=temp)i++;
+        swap(&arr[i],&arr[j]);
+    }
+    quickSort(arr,l,i-1);
+    quickSort(arr,i+1,r);
+}
+int main(){
+    int arr[]={64, 34, 25, 12, 22, 11, 90};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr,0,n-1);
+    for(int i=0;i<n;i++)printf("%d ",arr[i]);
+    return 0;
+}
